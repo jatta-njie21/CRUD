57fbde1fe157f7d05063c972f104a72aa5a4b67b
jatta-njie21 <laboyjatta@gmail.com>
2021-01-09 23:00:37 +0000

    #Joe004

---
+++ Main.py
@@ -0,0 +1,18 @@
+from Connection import SqliteConnection
+
+class MainCrud:
+    Connection.SqliteConnection("crud")
+    
+    def UpdateRecord():
+        pass
+    
+    def AddRecord():
+        pass
+    
+    def DeleteRecord():
+        pass
+    
+    def SearchRecord():
+        pass
+    
+        
